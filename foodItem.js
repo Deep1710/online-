a16ca8821159916862380a4f923f966a3fc43be0
Deep1710 <132344067+Deep1710@users.noreply.github.com>
2023-07-03 11:28:45 +0530

    Add files via upload

---
+++ foodItem.js
@@ -0,0 +1,347 @@
+const foodItem = [
+
+    {
+        id: 1,
+       name: 'Ambur',
+       category : 'biryani',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 2,
+       name: 'Ambura',
+       category : 'biryani',
+       price : 15,
+       img: 'food2.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 3,
+       name: 'Amburb',
+       category : 'biryani',
+       price : 15,
+       img: 'food3.jpg',
+       quantity: 1
+    },
+    {
+        id: 4,
+       name: 'Amburc',
+       category : 'biryani',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 5,
+       name: 'Amburcb',
+       category : 'biryani',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id:  6,
+       name: 'Amburcb',
+       category : 'biryani',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 7,
+       name: 'Amburcd',
+       category : 'biryani',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 8,
+       name: 'Amburd',
+       category : 'italian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 9,
+       name: 'Ambure',
+       category : 'italian',
+       price: 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 10,
+       name: 'Amburei',
+       category : 'italian',
+       price: 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 11,
+       name: 'Amburei',
+       category : 'italian',
+       price: 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 12,
+       name: 'Amburei',
+       category : 'italian',
+       price: 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 13,
+       name: 'Amburei',
+       category : 'italian',
+       price: 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+
+    {
+        id: 14,
+       name: 'Amburf',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 15,
+       name: 'Amburg',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 16,
+       name: 'Amburgc',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 17,
+       name: 'Amburgc',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 18,
+       name: 'Amburgc',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 19,
+       name: 'Amburgc',
+       category : 'chicken',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 20,
+       name: 'Amburh',
+       category : 'southIndian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 21,
+       name: 'Ambur1',
+       category : 'southIndian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 22,
+       name: 'Amburj',
+       category : 'southIndian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 23,
+       name: 'Amburk',
+       category : 'southIndian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 24,
+       name: 'Amburkj',
+       category : 'southIndian',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+ 
+    {
+        id: 25,
+       name: 'paneerl',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 26,
+       name: 'paneerlp',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 27,
+       name: 'paneerlq',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 28,
+       name: 'Amburm',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 29,
+       name: 'Amburn',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 30,
+       name: 'Amburo',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 31,
+       name: 'Amburp',
+       category : 'paneer',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+  
+    {
+        id: 32,
+       name: 'Amburq',
+       category : 'vegetable',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 33,
+       name: 'Amburr',
+       category : 'vegetable',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 34,
+       name: 'Amburs',
+       category : 'vegetable',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 35,
+       name: 'Amburt',
+       category : 'vegetable',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 36,
+       name: 'Amburt',
+       category : 'special',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+    {
+        id: 37,
+       name: 'Amburm',
+       category : 'special',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 38,
+       name: 'Amburz',
+       category : 'special',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+    {
+        id: 39,
+       name: 'Amburg',
+       category : 'special',
+       price : 15,
+       img: 'food1.jpg',
+       quantity: 1
+    },
+
+
+]
+
+export {foodItem};
